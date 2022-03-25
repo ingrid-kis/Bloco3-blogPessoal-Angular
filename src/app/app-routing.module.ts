0399407a8181da045b54e6b88ab9d7a0b59e6fd3
Ingrid Kis <ingrid.kr@hotmail.com>
2022-03-25 12:16:53 -0300

    @ingrid-kis [TASK: Components Routes]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignComponent } from './sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"", redirectTo: "login", pathMatch: "full"},
+  
+  {path: "login", component: LoginComponent},
+  {path: "menu", component: MenuComponent},
+  {path: "sign", component: SignComponent},
+  {path: "footer", component: FooterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
